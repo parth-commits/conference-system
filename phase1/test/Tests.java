23c93b54ec3a9893ddf9953f91cb9af3ae532f46
Parth Patel <parthpatel1087@gmail.com>
2020-10-31 10:42:11 -0400

    test of test folder - parth

---
+++ phase1/test/Tests.java
@@ -0,0 +1,53 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Tests {
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // test constructor
+    @Test(timeout=50)
+    public void testEmptyConstructor() {
+        new MyHashing();
+    }
+
+    // test constructor
+    @Test(timeout=50)
+    public void testConstructor() {
+        new MyHashing(70);
+    }
+
+    // test hashing(int)
+    @Test(timeout=50)
+    public void testHashing1() {
+        MyHashing h = new MyHashing();
+        assertEquals("Should return the previous seed value\n", 100, h.hash(40));
+    }
+
+    // test hashing(int)
+    @Test(timeout=50)
+    public void testHashing2() {
+        MyHashing h = new MyHashing(7);
+        assertEquals("Should return the previous seed value\n", 7, h.hash(40));
+        assertEquals("Should store an input value into seed\n", 40, h.hash(100));
+    }
+
+    // test hashing(String) with empty String
+    @Test(timeout=50)
+    public void testHashing3() {
+        MyHashing h = new MyHashing();
+        assertEquals("Should return the previous seed value\n", 0, h.hash(""));
+    }
+
+    // test hashing(String)
+    @Test(timeout=50)
+    public void testHashing4() {
+        MyHashing h = new MyHashing(7);
+        assertEquals("Should return the previous seed value\n", 317, h.hash("ted"));
+    }
+}
