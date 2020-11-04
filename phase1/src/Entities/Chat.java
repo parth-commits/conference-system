941b71275c740d9ad55d0028344983d222bcfdee
matthew.she <matthew.she@mail.utoronto.ca>
2020-11-03 21:00:13 -0500

    Chat and message

--- phase1/src/Entities/Chat.java
+++ phase1/src/Entities/Chat.java
@@ -1,4 +1,32 @@
 package Entities;
 
 public class Chat {
+    private int id1;
+    private int id2;
+    private List<Message> messages;
+
+    public Chat(int id1, int id2){
+        this.id1 = id1;
+        this.id2 = id2;
+        messages = new ArrayList<>();
+    }
+
+    public int getId1() {
+        return id1;
+    }
+
+    public int getId2() {
+        return id2;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void addMessages(int sender, int recipient, String context){
+        newMessage = Message(sender, recipient, context);
+        messages.add(newMessage);
+    }
+
+    //Message search???
 }
--- phase1/src/Entities/Message.java
+++ phase1/src/Entities/Message.java
@@ -1,4 +1,33 @@
 package Entities;
+import Java.util.Date;
+import java.util.String;
 
 public class Message {
+    private int sender;
+    private int recipient;
+    private String context;
+    private Date time;
+
+    public Message(int sender, int recipient, String context){
+        this.sender = sender;
+        this.recipient = recipient;
+        this.context = context;
+        this.time = Date();
+    }
+
+    public int getSender() {
+        return sender;
+    }
+
+    public int getRecipient() {
+        return recipient;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public Time getTime() {
+        return time;
+    }
 }
