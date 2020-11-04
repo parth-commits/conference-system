d67c0a734b132fa2225239444693f4bad2386a48
hoyun <yun.ho@mail.utoronto.ca>
2020-11-04 15:52:15 +0800

    Draft for methods and variables in Attendee

--- phase1/src/Entities/Attendee.java
+++ phase1/src/Entities/Attendee.java
@@ -1,4 +1,64 @@
 package Entities;
 
+import java.util.ArrayList;
+
 public class Attendee {
+
+    private Integer user_id;
+    private String passwords;
+    private String name;
+    private ArrayList<String> contacts;
+    private ArrayList<String> signedUpEvents;
+
+    public Attendee(Integer user_id, String name, String passwords){
+    this.user_id = user_id;
+    this.name = name;
+    this.passwords = passwords;
+    this.contacts = new ArrayList<>();
+    this.signedUpEvents = new ArrayList<>();
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getPasswords() {
+        return passwords;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getContacts() {
+        return contacts;
+    }
+
+    public ArrayList<String> getSignedUpEvents() {
+        return signedUpEvents;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPasswords(String passwords) {
+        this.passwords = passwords;
+    }
+
+    public void addContact(Integer user_id){
+        this.contacts.add(Integer.toString(user_id));
+    }
+
+    public boolean removeContact(Integer user_id){
+        return this.contacts.remove(Integer.toString(user_id));
+    }
+
+    public void addEvent(String eventTitle){
+        this.signedUpEvents.add(eventTitle);
+    }
+
+    public boolean removeEvent(String eventTitle){
+        return this.signedUpEvents.remove(eventTitle);
+    }
 }
