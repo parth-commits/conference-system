fa77a731209cc081cb1a816ba6b9c80b7ddc00db
hoyun <yun.ho@mail.utoronto.ca>
2020-11-05 21:29:20 +0800

    Fix errors in ChatManager

--- phase1/src/UseCases/ChatManager.java
+++ phase1/src/UseCases/ChatManager.java
@@ -1,33 +1,36 @@
 package UseCases;
+import Entities.Chat;
+
 import java.util.ArrayList;
 
 public class ChatManager {
-    private List<Chat> chats;
+    private ArrayList<Chat> chats;
 
     public ChatManager(){
-        chats = new ArrayList<Chat>();
+        chats = new ArrayList<>();
     }
 
     public void addMessageToChat(int sender, int recipient, String context){
-        chat = findChat(sender, recipient);
+        Chat chat = findChat(sender, recipient);
         chat.addMessages(sender, recipient, context);
     }
 
     private void createChat(int id1, int id2){
-        chats.add(Chat(id1, id2));
+        chats.add(new Chat(id1, id2));
     }
 
 
     public Chat findChat(int id1, int id2){
-        int temp = 0;
+        int temp;
         if (id1 > id2){
             temp = id2;
             id1 = id2;
             id2 = temp;
         }
-        for(int i = 0; i<len(chats); i++){
-            if (chats[i].getId1() == id1) && (chats[i].getId2() == id2))
-                    return chats.get(i);
+        for (Chat chat : chats) {
+            if ((chat.getId1() == id1) && (chat.getId2() == id2)) {
+                return chat;
+            }
         }
         //adds chat if does not exists
         createChat(id1, id2);
