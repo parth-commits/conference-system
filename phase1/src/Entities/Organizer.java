fb8f2f1b6c96c3709380de2bbf80941a53d6fc2e
Parth Patel <parthpatel1087@gmail.com>
2020-11-07 23:44:30 -0500

    Organizer.java and OrganizerManager.java updated by parth

--- phase1/src/Entities/Organizer.java
+++ phase1/src/Entities/Organizer.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public class Organizer extends Attendee{
 
+    //list of all contacts of this Organizer
+    private ArrayList<String> contacts;
+
+    //list of all events that this Organizer signed up for
+    private ArrayList<Integer> signedUpEvents;
+
     //list of all speakers created
     private ArrayList<String> speakersCreated;
 
@@ -12,12 +18,14 @@ public class Organizer extends Attendee{
 
     public Organizer(String user_id, String name, String passwords) {
         super(user_id, name, passwords);
+        this.contacts = new ArrayList<>();
+        this.signedUpEvents = new ArrayList<>();
         this.speakersCreated = new ArrayList<>();
         this.eventsCreated = new ArrayList<>();
     }
 
-    public void addSpeakerCreated(Integer user_id){
-        this.speakersCreated.add(Integer.toString(user_id));
+    public void addSpeakerCreated(String user_id){
+        this.speakersCreated.add(user_id);
     }
 
     public void addEventCreated(String event_id){
@@ -28,4 +36,27 @@ public class Organizer extends Attendee{
         return this.eventsCreated.remove(event_id);
     }
 
+    public ArrayList<String> getContacts() {
+        return contacts;
+    }
+
+    public boolean addContact(String user_id){
+        return this.contacts.add(user_id);
+    }
+
+    public boolean removeContact(String user_id){
+        return this.contacts.remove(user_id);
+    }
+
+    public ArrayList<Integer> getSignedUpEvents() {
+        return signedUpEvents;
+    }
+
+    public void addEvent(Integer eventTitle){
+        this.signedUpEvents.add(eventTitle);
+    }
+
+    public boolean removeEvent(String eventTitle){
+        return this.signedUpEvents.remove(eventTitle);
+    }
 }
