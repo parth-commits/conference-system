798cfe5b834da7561bc927f9bb6b5e8451446761
Parth Patel <parthpatel1087@gmail.com>
2020-11-08 17:43:10 -0500

    made some changes to the files, by parth and tanmay

--- phase1/src/Entities/Organizer.java
+++ phase1/src/Entities/Organizer.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 
 public class Organizer extends Attendee{
 
-    //list of all contacts of this Organizer
-    private ArrayList<String> contacts;
-
-    //list of all events that this Organizer signed up for
-    private ArrayList<Integer> signedUpEvents;
-
     //list of all speakers created
     private ArrayList<String> speakersCreated;
 
@@ -18,8 +12,6 @@ public class Organizer extends Attendee{
 
     public Organizer(String user_id, String name, String passwords) {
         super(user_id, name, passwords);
-        this.contacts = new ArrayList<>();
-        this.signedUpEvents = new ArrayList<>();
         this.speakersCreated = new ArrayList<>();
         this.eventsCreated = new ArrayList<>();
     }
@@ -36,27 +28,4 @@ public class Organizer extends Attendee{
         return this.eventsCreated.remove(event_id);
     }
 
-    public ArrayList<String> getContacts() {
-        return contacts;
-    }
-
-    public boolean addContact(String user_id){
-        return this.contacts.add(user_id);
-    }
-
-    public boolean removeContact(String user_id){
-        return this.contacts.remove(user_id);
-    }
-
-    public ArrayList<Integer> getSignedUpEvents() {
-        return signedUpEvents;
-    }
-
-    public void addEvent(Integer eventTitle){
-        this.signedUpEvents.add(eventTitle);
-    }
-
-    public boolean removeEvent(String eventTitle){
-        return this.signedUpEvents.remove(eventTitle);
-    }
 }
