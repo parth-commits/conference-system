bcf9675b8b3c38806a08c3b82a9a92e174f9520d
jackkkji <jackjipipixia@gmail.com>
2020-11-13 19:20:27 -0500

    attendeesystem adding methods

--- phase1/src/Controller/AttendeeSystem.java
+++ phase1/src/Controller/AttendeeSystem.java
@@ -16,7 +16,7 @@ public class AttendeeSystem {
     private RoomManager roomManager;
     private EventManager eventManager;
 
-    public AttendeeSystem (AttendeeManager attendeeManager, RoomManager roomManager, EventManager eventManager){
+    public AttendeeSystem(AttendeeManager attendeeManager, RoomManager roomManager, EventManager eventManager) {
         this.attendeeManager = attendeeManager;
         this.roomManager = roomManager;
         this.eventManager = eventManager;
@@ -26,5 +26,103 @@ public class AttendeeSystem {
     }
 
     public boolean start(String userID) {
+        while (true) {
+            boolean validInput = false;
+            i = input.getKeyboardInput()
+            if (i.equals("1")) {
+                message(userID);
+            }
+            else if (i.equals("2")) {
+                addRemoveContact(userID);
+            }
+            else if (o.equals("3")){
+                joinLeaveEvent();
+            }
+            else if (o.equals("4")){
+                return false;
+            }
+            else if (o.equals("4")){
+                return true;
+            }
+        }
     }
+
+
+    private void message(String userID) {
+        messageSystem.sendMessage(userID);
+    }
+
+
+
+
+    private void joinLeaveEvent() {
+    }
+
+
+
+    private boolean userExists(String userid){
+        if (attendeeManager.userExist(userid)||organizerManager.userExist(userid)||speakerManager.userExist(userid)){
+            return true;
+        }
+        return false;
+    }
+
+
+
+    private void addRemoveContact(String userID) {
+        output.addRemoveContact();
+        String option = input.getKeyboardInput();
+        while (!(option.equals("1")||option.equals("2"))){
+            output.addRemoveContact();
+            option = input.getKeyboardInput();
+        }
+        if (option.equals("1")){
+            output.enterContactUserid(false);
+            String input = this.input.getKeyboardInput();
+            while (!(organizerManager.userExist(input)||attendeeManager.userExist(input)||speakerManager.userExist(input))){
+                output.enterContactUserid(true);
+                input = this.input.getKeyboardInput();
+            }
+            organizerManager.addContact(userID, input);
+            if (organizerManager.userExist(input)){
+                organizerManager.addContact(input, userID);
+            }
+            else if (attendeeManager.userExist(input)){
+                attendeeManager.addContact(input, userID);
+            }
+            else{
+                speakerManager.addContact(input, userID);
+            }
+            chatManager.createChat(input, userID);
+        }
+        else {
+            output.enterContactUserid(false);
+            String input = this.input.getKeyboardInput();
+            while (!(organizerManager.userExist(input)||attendeeManager.userExist(input)||speakerManager.userExist(input))){
+                output.enterContactUserid(true);
+                input = this.input.getKeyboardInput();
+            }
+            organizerManager.removeContact(userID, input);
+            if (organizerManager.userExist(input)){
+                organizerManager.removeContact(input, userID);
+            }
+            else if (attendeeManager.userExist(input)){
+                attendeeManager.removeContact(input, userID);
+            }
+            else{
+                speakerManager.removeContact(input, userID);
+            }
+            chatManager.deleteChat(input,userID);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 }
