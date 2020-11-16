2ce3419d105c71c32d786752cc743f32852d17a6
Parth Patel <parthpatel1087@gmail.com>
2020-11-15 19:12:32 -0500

    messaging functionality updated

---
+++ phase1/src/ConferenceDemo.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public class ConferenceDemo {
+    public static void main(String[] args) throws IOException, ParseException {
+        ConferenceSystem conferenceSystem = new ConferenceSystem();
+        conferenceSystem.run();
+        //should work
+    }
+}
