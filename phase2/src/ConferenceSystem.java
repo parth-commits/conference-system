139869a3f87b70960321aae61fec96bed723cae6
Parth Patel <parthpatel1087@gmail.com>
2020-11-25 00:12:36 -0500

    debugger moved outside controller

--- phase2/src/ConferenceSystem.java
+++ phase2/src/ConferenceSystem.java
@@ -80,7 +80,8 @@ public class ConferenceSystem {
     public void run () throws IOException, ParseException {
         deletePastEvents();
         boolean shutdown = false;
-        listofIDPass();
+        Debugger debugger = new Debugger();
+        debugger.printStateofSystem(organizerManager,speakerManager,attendeeManager,eventManager,roomManager);
         while (!shutdown) {
             String userID = logInAndRegistrationSystem.start();
             if (userID.equals("SHUTDOWN")){
@@ -95,6 +96,7 @@ public class ConferenceSystem {
             else {
                 shutdown = speakerSystem.start(userID);
             }
+            saveState();
         }
         saveState();
     }
@@ -132,36 +134,6 @@ public class ConferenceSystem {
                 eventManager.removeEvent(eventID);
             }
         }
-    }
-
-    private void listofIDPass() {
-        System.out.println("\n\n\nNOTE: this is a debug feature to assist our TA. Here is the current state of the system:");
-        System.out.println("\n\nAll the organizers in the system:");
-        ArrayList<String> organizersList = organizerManager.getUserIDs();
-        for (String organizer: organizersList){
-            System.out.println(organizerManager.getOrganizer(organizer).toString());
-        }
-        System.out.println("\n\nAll the speakers in the system:");
-        ArrayList<String> speakers = speakerManager.getUserIDs();
-        for (String speaker: speakers){
-            System.out.println(speakerManager.getSpeaker(speaker).toString());
-        }
-        System.out.println("\n\nAll the attendees in the system:");
-        ArrayList<String> attendees = attendeeManager.getUserIDs();
-        for (String attendee: attendees){
-            System.out.println(attendeeManager.getAttendee(attendee).toString());
-        }
-        System.out.println("\n\nAll the events in the system:");
-        ArrayList<Integer> eventIDS = eventManager.getListOfEventIDs();
-        for (Integer event: eventIDS){
-            System.out.println(eventManager.getEvent(event).toString());
-        }
-        System.out.println("\n\nAll the rooms in the system");
-        ArrayList<String> roomLOCS = roomManager.getAllRoomLocs();
-        for (String room: roomLOCS){
-            System.out.println(roomManager.getRoom(room).toString());
-        }
-        System.out.println("\n\n");
     }
 }
 
---
+++ phase2/src/Debugger.java
@@ -0,0 +1,38 @@
+import Entities.Attendee;
+import UseCases.*;
+
+import java.util.ArrayList;
+
+public class Debugger {
+    public Debugger(){}
+
+    public void printStateofSystem(OrganizerManager organizerManager, SpeakerManager speakerManager, AttendeeManager attendeeManager, EventManager eventManager, RoomManager roomManager){
+        System.out.println("\n\n\nNOTE: this is a debug feature to assist our TA. Here is the current state of the system:");
+        System.out.println("\n\nAll the organizers in the system:");
+        ArrayList<String> organizersList = organizerManager.getUserIDs();
+        for (String organizer: organizersList){
+            System.out.println(organizerManager.getOrganizer(organizer).toString());
+        }
+        System.out.println("\n\nAll the speakers in the system:");
+        ArrayList<String> speakers = speakerManager.getUserIDs();
+        for (String speaker: speakers){
+            System.out.println(speakerManager.getSpeaker(speaker).toString());
+        }
+        System.out.println("\n\nAll the attendees in the system:");
+        ArrayList<String> attendees = attendeeManager.getUserIDs();
+        for (String attendee: attendees){
+            System.out.println(attendeeManager.getAttendee(attendee).toString());
+        }
+        System.out.println("\n\nAll the events in the system:");
+        ArrayList<Integer> eventIDS = eventManager.getListOfEventIDs();
+        for (Integer event: eventIDS){
+            System.out.println(eventManager.getEvent(event).toString());
+        }
+        System.out.println("\n\nAll the rooms in the system");
+        ArrayList<String> roomLOCS = roomManager.getAllRoomLocs();
+        for (String room: roomLOCS){
+            System.out.println(roomManager.getRoom(room).toString());
+        }
+        System.out.println("\n\n");
+    }
+}
