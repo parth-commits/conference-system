845bd82b63568924c5fcedc448a0d613b07df529
Hao-te WANG <harold.wang@mail.utoronto.ca>
2020-12-01 12:00:25 +0800

    Java doc of Entitiy Request

--- phase2/src/Entities/Request.java
+++ phase2/src/Entities/Request.java
@@ -2,20 +2,28 @@ package Entities;
 
 import java.io.Serializable;
 
+
+/**
+ * Request Entities simply records the id of request, the attendee who made the request, the request context and the status.
+ * @author Group_0112
+ * @version 1.0
+ * @since November 16th, 2020
+ */
+
 public class Request implements Serializable {
 
-    //the id of the request
+
     private int id;
-
-    //the userid of the attendee who made the req
     private String user_id;
-
-    //the req string
     private String req;
+    private String status;  //status of the req
 
-    //status of the req
-    private String status;
-
+    /**
+     * Constructor
+     * @param id the id of the request
+     * @param user_id the userid of the attendee who made the request
+     * @param req the req string
+     */
     public Request( int id, String user_id, String req){
         this.id = id;
         this.user_id = user_id;
@@ -23,38 +31,74 @@ public class Request implements Serializable {
         this.status = "Pending";
     }
 
+    /**
+     * Gets the id of the request
+     * @return int The id of the request
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *Sets the id of the request
+     * @param id the id of the request we want to set
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the user_id of the attendee who made the request
+     * @return String user_id
+     */
     public String getUser_id() {
         return user_id;
     }
 
+    /**
+     * Sets the user_id of the attendee who made the request
+     * @param user_id the user_id of the attendee who made the request
+     */
     public void setUser_id(String user_id) {
         this.user_id = user_id;
     }
 
+    /**
+     * Gets the request context
+     * @return String the request context
+     */
     public String getReq() {
         return req;
     }
 
+    /**
+     * Sets the request context
+     * @param req the request context
+     */
     public void setReq(String req) {
         this.req = req;
     }
 
+    /**
+     * Gets the status of the request
+     * @return String the status of the request
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Sets the status of the request
+     * @param status the status of the request
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Returns the request in String form, following by request id, attendee who made the request, the request context and status
+     * @return String the request in String form
+     */
     @Override
     public String toString() {
         return "Request ID: " + id + " | Attendee that made request: " + user_id + " | Request: " + req + " | Status: " + status;
