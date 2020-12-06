f8cf6390ee6759e65b7aa2d4b0c2da4cd460ea87
Ray <yenray.wen@gmail.com>
2020-12-06 17:17:22 +0800

    Added two files to create separate system for export system testing.

---
+++ phase2/src/TestExportDemo.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * The TestExportDemo class contains just one method that helps this program to create
+ * a new TestExportSystem and run.
+ * @author Group_0112
+ * @version 2.0
+ * @since December 6th, 2020
+ */
+public class TestExportDemo {
+    public static void main(String[] args) throws IOException {
+        TestExportSystem exportSystem = new TestExportSystem();
+        exportSystem.run();
+    }
+}
